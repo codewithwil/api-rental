0b13680971767acbf6919b9b11a1bba4bd7516da
codewithwil <156795712+codewithwil@users.noreply.github.com>
2025-08-19 10:54:37 +0700

    Update category.php

--- routes/api/resources/assets/category.php
+++ routes/api/resources/assets/category.php
@@ -4,12 +4,12 @@ use App\Http\Controllers as ctr;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['role:admin|supervisor|employee'])->group(function () {
-    Route::get("/", [ctr\API\Resources\Category\Categoryc::class, 'index'])->name("index");
-    Route::get("/create", [ctr\API\Resources\Category\Categoryc::class, 'create'])->name("create");
-    Route::get("/invoice", [ctr\API\Resources\Category\Categoryc::class, 'invoice'])->name("invoice");
-    Route::get("/show/{id}", [ctr\API\Resources\Category\Categoryc::class, 'show'])->name("show");
-    Route::get("/edit/{id}", [ctr\API\Resources\Category\Categoryc::class, 'edit'])->name("edit");
-    Route::post("/store", [ctr\API\Resources\Category\Categoryc::class, 'store'])->name("store");
-    Route::post("/update/{id}", [ctr\API\Resources\Category\Categoryc::class, 'update'])->name("update");
-    Route::post("/delete/{id}", [ctr\API\Resources\Category\Categoryc::class, 'delete'])->name("delete");
+    Route::get("/", [ctr\API\Resources\Category\CategoryC::class, 'index'])->name("index");
+    Route::get("/create", [ctr\API\Resources\Category\CategoryC::class, 'create'])->name("create");
+    Route::get("/invoice", [ctr\API\Resources\Category\CategoryC::class, 'invoice'])->name("invoice");
+    Route::get("/show/{id}", [ctr\API\Resources\Category\CategoryC::class, 'show'])->name("show");
+    Route::get("/edit/{id}", [ctr\API\Resources\Category\CategoryC::class, 'edit'])->name("edit");
+    Route::post("/store", [ctr\API\Resources\Category\CategoryC::class, 'store'])->name("store");
+    Route::post("/update/{id}", [ctr\API\Resources\Category\CategoryC::class, 'update'])->name("update");
+    Route::post("/delete/{id}", [ctr\API\Resources\Category\CategoryC::class, 'delete'])->name("delete");
 });
