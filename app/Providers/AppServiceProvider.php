ce5cb16e64a7a7fbdbd97000ad43ec62b873f1b9
codewithwil <willyrandika0603@gmail.com>
2025-08-19 10:14:59 +0700

    minor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-
+        if (app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
