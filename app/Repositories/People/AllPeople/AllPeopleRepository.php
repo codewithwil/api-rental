68da8e6d7b119a6f66a798ba6533ffe8a81701f1
codewithwil <willyrandika0603@gmail.com>
2025-08-26 10:59:00 +0700

    minor

--- app/Repositories/People/AllPeople/AllPeopleRepository.php
+++ app/Repositories/People/AllPeople/AllPeopleRepository.php
@@ -11,6 +11,16 @@ class AllPeopleRepository implements AllPeopleRepositoryInterface
 
     public function getAll()
     {
-        return User::get();
+        return User::with(['roles'])
+            ->get()
+            ->map(function ($user) {
+                return [
+                    'email'             => $user->email,
+                    'level'             => $user->roles->pluck('name')->first(), 
+                    'last_login_ip'     => $user->last_login_ip,
+                    'last_login_device' => $user->last_login_device,
+                    'last_active_at'    => $user->last_active_at,
+                ];
+            });
     }
 }
