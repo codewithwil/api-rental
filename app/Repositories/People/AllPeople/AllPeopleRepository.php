5508dc74ca3acfb53b0cd5c9de66538fd30df1d5
codewithwil <willyrandika0603@gmail.com>
2025-09-30 13:40:30 +0700

    solv paginate

--- app/Repositories/People/AllPeople/AllPeopleRepository.php
+++ app/Repositories/People/AllPeople/AllPeopleRepository.php
@@ -8,19 +8,31 @@ use App\{
 
 class AllPeopleRepository implements AllPeopleRepositoryInterface
 {
-
     public function getAll()
     {
-        return User::with(['roles'])
-            ->get()
-            ->map(function ($user) {
-                return [
-                    'email'             => $user->email,
-                    'level'             => $user->roles->pluck('name')->first(), 
-                    'last_login_ip'     => $user->last_login_ip,
-                    'last_login_device' => $user->last_login_device,
-                    'last_active_at'    => $user->last_active_at,
-                ];
-            });
+        $perPage = 10;
+        $page = request()->get('page', 1);
+
+        $users = User::with(['roles'])->get();
+
+        $mapped = $users->map(function ($user) {
+            return [
+                'email'             => $user->email,
+                'level'             => $user->roles->pluck('name')->first(),
+                'last_login_ip'     => $user->last_login_ip,
+                'last_login_device' => $user->last_login_device,
+                'last_active_at'    => $user->last_active_at,
+            ];
+        });
+
+        $paginated = \Illuminate\Pagination\LengthAwarePaginator::make(
+            $mapped->forPage($page, $perPage),
+            $mapped->count(),
+            $perPage,
+            $page
+        );
+
+        return $paginated;
     }
+
 }
